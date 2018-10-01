1793a8c5dc3a860ccf1040fdac8acd9563c40062
Jens Daniel Kramhøft <jdkramhoft@gmail.com>
2018-10-01 15:25:43 +0200

    Tilføjede kommentar Dice.java

--- src/terning/Dice.java
+++ src/terning/Dice.java
@@ -8,7 +8,9 @@ public class Dice {
 		int d3=Math.round(d2);             // 0-5 integer
 		return d3 + 1;                     // 1-6		
 	}
-	
+
+	//Testkommentar
+
 	// roll the die n times and print the values
 	public void rollMultiple(int n) {
 		for (int i=1; i<n; i++) {
