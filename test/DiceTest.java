855c4299f5f9930a585084d969eaeeb591ed01a5
Kaloyan Penov <s133967@student.dtu.dk>
2018-10-01 15:43:35 +0200

    - adding empty test

---
+++ test/DiceTest.java
@@ -0,0 +1,22 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    public void nothing(){
+        assertTrue(true);
+    }
+
+}
