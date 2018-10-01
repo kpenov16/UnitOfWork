627e4722d56ed69dfa4645c79e3446fabbaa49d9
Kaloyan Penov <s133967@student.dtu.dk>
2018-10-01 16:10:54 +0200

    - test if 1 has the required occurrence after 60_000 rolls

--- test/DiceTest.java
+++ test/DiceTest.java
@@ -1,6 +1,10 @@
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import terning.Dice;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -8,6 +12,7 @@ class DiceTest {
 
     @BeforeEach
     void setUp() {
+
     }
 
     @AfterEach
@@ -15,8 +20,28 @@ class DiceTest {
     }
 
     @Test
-    public void nothing(){
-        assertTrue(true);
+    public void given60000Rolls_returnAbout10000TimesPerSide(){
+        Dice dice = new Dice();
+        String rolls = "";
+        int baseExpectedOccurrances = 10_000;
+        int deviation = 400;
+        int n = 60000;
+        for(int i = 0; i < n; i++){
+            rolls += dice.roll();
+        }
+
+        Pattern pattern = Pattern.compile("1");
+        Matcher matcherOne = pattern.matcher(rolls);
+        int countOneces = 0;
+        while (matcherOne.find())
+            countOneces++;
+
+        System.out.println(rolls);
+        System.out.println(countOneces);
+
+        assertTrue(countOneces >= (baseExpectedOccurrances - deviation)
+                         && countOneces <= (baseExpectedOccurrances + deviation)
+                  );
     }
 
 }
